5fa14957b2eb4eca47bfb47ea3d15e1c23ba7ea5
samimh23 <sami.mahjoub@esprit.tn>
2026-09-16 10:31:58 +0100

    feat: authenticate inbound email domain

--- azure/environments/staging/email-ingestion-dns.tf
+++ azure/environments/staging/email-ingestion-dns.tf
@@ -22,3 +22,47 @@ resource "azurerm_dns_mx_record" "brevo_inbound" {
 
   tags = local.tags
 }
+
+resource "azurerm_dns_txt_record" "brevo_verification" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.email_ingestion.name
+  resource_group_name = azurerm_resource_group.this.name
+  ttl                 = 1800
+
+  record {
+    value = "brevo-code:1753947e2d761505660e27769db01ea6"
+  }
+
+  tags = local.tags
+}
+
+resource "azurerm_dns_cname_record" "brevo_dkim_1" {
+  name                = "brevo1._domainkey"
+  zone_name           = azurerm_dns_zone.email_ingestion.name
+  resource_group_name = azurerm_resource_group.this.name
+  ttl                 = 1800
+  record              = "b1.inbox-fiscora-me.dkim.brevo.com"
+  tags                = local.tags
+}
+
+resource "azurerm_dns_cname_record" "brevo_dkim_2" {
+  name                = "brevo2._domainkey"
+  zone_name           = azurerm_dns_zone.email_ingestion.name
+  resource_group_name = azurerm_resource_group.this.name
+  ttl                 = 1800
+  record              = "b2.inbox-fiscora-me.dkim.brevo.com"
+  tags                = local.tags
+}
+
+resource "azurerm_dns_txt_record" "brevo_dmarc" {
+  name                = "_dmarc"
+  zone_name           = azurerm_dns_zone.email_ingestion.name
+  resource_group_name = azurerm_resource_group.this.name
+  ttl                 = 1800
+
+  record {
+    value = "v=DMARC1; p=none; rua=mailto:rua@dmarc.brevo.com"
+  }
+
+  tags = local.tags
+}
